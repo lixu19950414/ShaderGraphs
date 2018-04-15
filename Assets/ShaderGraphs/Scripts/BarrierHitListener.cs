40b062c7abb5c707c18dd62eac3e9c8e616a358b
傲娇的露易丝 <980858415@qq.com>
2018-04-15 20:41:19 +0800

    Add trigger events

--- Assets/ShaderGraphs/Scripts/BarrierHitListener.cs
+++ Assets/ShaderGraphs/Scripts/BarrierHitListener.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BarrierHitListener : MonoBehaviour {
+    private Vector3 objectSpacePos;
+    private Material mat;
+
+    private void Start()
+    {
+        mat = gameObject.GetComponent<MeshRenderer>().material;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("OnTriggerEnter " + other.name);
+        objectSpacePos = gameObject.transform.worldToLocalMatrix * new Vector4(0.0f, 1.0f, 0.0f, 1.0f);
+        Debug.LogFormat("{0}, {1}, {2}", objectSpacePos.x, objectSpacePos.y, objectSpacePos.z);
+        mat.SetVector("Vector3_C18AA2D4", new Vector4(objectSpacePos.x, objectSpacePos.y, objectSpacePos.z, 0.0f));
     }
 
     private void OnTriggerStay(Collider other)
