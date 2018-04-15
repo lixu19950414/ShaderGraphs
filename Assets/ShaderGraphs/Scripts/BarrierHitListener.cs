e5fdf5bd27bbc340eb6e913b3ad44dd9778013b2
傲娇的露易丝 <980858415@qq.com>
2018-04-15 17:24:08 +0800

    Add simple physics

---
+++ Assets/ShaderGraphs/Scripts/BarrierHitListener.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrierHitListener : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter " + other.name);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        // Debug.Log("OnTriggerStay " + other.name);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("OnTriggerExit " + other.name);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("OnCollisionEnter");
+    }
+}
